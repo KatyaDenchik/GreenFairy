bcc50cb758fd81b386f8150a631081bf755a4e91
USER-PC\User <katyadenchik11@gmail.com>
2023-04-09 21:43:54 +0300

    no message

--- GreenFairy/GreenFairy/Data/Authentication/AnonUserAccaunt.cs
+++ GreenFairy/GreenFairy/Data/Authentication/AnonUserAccaunt.cs
@@ -2,12 +2,26 @@
 
 namespace GreenFairy.Data.Authentication
 {
-    public class AnonUserAccaunt: UserAccount
+    public class AnonUserAccaunt : UserAccount
     {
-        public List<PlantEntity> Plants { get; } = new List<PlantEntity>();
+        public OrderEntity CurentOrder { get; } = new();
         public AnonUserAccaunt()
         {
             Role = "Anon";
+        }
+
+        public void AddPlant(PlantEntity plant)
+        {
+            plant.Amount = 1;
+            if (CurentOrder.Plants.Any(s=>s.Name== plant.Name))
+            {
+                var oldPlant = CurentOrder.Plants.First(s => s.Name == plant.Name);
+                oldPlant.Amount++;
+            }
+            else
+            {
+                CurentOrder.Plants.Add(plant);
+            }
         }
     }
 }
